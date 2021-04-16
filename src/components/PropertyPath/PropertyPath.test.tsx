1dc0c1a8f840809d1a5e28c759005e8b3c0ba1fc
Artur Pryka <artur.pryka@keen.io>
2021-04-16 15:12:43 +0200

    test: 💍 add test for PropertyPath

---
+++ src/components/PropertyPath/PropertyPath.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+
+import PropertyPath from './PropertyPath';
+
+const render = (overProps: any = {}) => {
+  const props = {
+    path: ['@property'],
+    ...overProps,
+  };
+
+  const wrapper = rtlRender(
+    <div data-testid="wrapper">
+      <PropertyPath {...props} />
+    </div>
+  );
+
+  return {
+    wrapper,
+    props,
+  };
+};
+
+test('renders single property name', () => {
+  const {
+    wrapper: { getByText },
+    props,
+  } = render();
+
+  expect(getByText(props.path[0])).toBeInTheDocument();
+});
+
+test('renders multiple properties', () => {
+  const {
+    wrapper: { getByText },
+    props,
+  } = render({ path: ['property1', 'property2', 'property3'] });
+
+  props.path.forEach((p) => expect(getByText(p)).toBeInTheDocument());
+});
+
+test('renders multiple properties that overflow wrapper', () => {
+  Object.defineProperty(HTMLElement.prototype, 'scrollWidth', {
+    configurable: true,
+    value: 500,
+  });
+
+  const {
+    wrapper: { getByText, queryByText },
+    props,
+  } = render({ path: ['property1', 'property2', 'property3'] });
+
+  expect(queryByText(props.path[0])).toBeNull();
+  expect(queryByText(props.path[1])).toBeNull();
+  expect(getByText(props.path[2])).toBeInTheDocument();
+});
